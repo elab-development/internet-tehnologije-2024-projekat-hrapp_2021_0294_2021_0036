2cd465eede91066a00e7a12215bcf20ea15925f1
Dejana Savatic <savaticdejana@gmail.com>
2025-06-10 22:58:51 +0200

    Instaliran Sanctum za autentifikaciju

--- hr-app/app/Models/User.php
+++ hr-app/app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory;
+
+    use HasApiTokens;
 
     use Notifiable;
 
